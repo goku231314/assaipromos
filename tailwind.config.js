4c9768222a567f3ecff7e676ff3b07dc35bc4996
goku231314 <brasilsports45@gmail.com>
2024-12-05 13:25:13 -0300

    subir assai

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        azul: '#034991',
+        laranja: '#ff7300',
+        cinza: '#2f2f2f',
+        amarelo: '#fade4b'
+      },
+      screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+};
